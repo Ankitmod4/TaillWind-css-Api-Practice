0fedac4f8ac64f387b4484e8dd16c40a74cdb082
Ankit Mod <ankit300mod@gmail.com>
2024-08-15 18:52:36 +0530

    data pushed

--- src/Api.jsx
+++ src/Api.jsx
@@ -37,12 +37,12 @@ const Api = () => {
         <BrowserRouter>
             <div className='flex flex-wrap w-auto h-auto '>
                 <div className='flex  h-full w-full justify-between bg-slate-800 '>
-            <Link to="/cart" className='block text-center mt-4 m-6'>
-                            <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105'>
+            <Link to="/cart" className='block text-center mt-4 m-6 '>
+                            <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105 '>
                                 Go to Cart
                             </button>
                         </Link>
-            <Link to="/" className='block text-center mt-4 m-6'>
+            <Link to="/" className='block text-center mt-4 m-6 '>
                             <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105'>
                                Go To Home
                             </button>
@@ -52,19 +52,18 @@ const Api = () => {
                 <Route path="/" element={
                     <>
                         {data.map((product, i) => (
-                            <div key={i} className='w-full sm:w-1/2 justify-center md:w-1/3 p-2 lg:w-1/4'>
-                                <h1>{product.id}</h1>
-                                <h2>{product.category}</h2>
-                                <h1>{product.price}</h1>
-                                <img src={product.image} alt="" width={200} />
+                            <div key={i} className='w-full sm:w-1/2 justify-center md:w-1/3 lg:w-1/4 p-10 transition duration-500 ease-in-out transform scale-75 hover:fill-white hover:scale-90 border border-gray-900' >
+                                <h2 className='text-3xl font-sans font-bold italic	'>{product.category}</h2>
+                                <h1 className='font-bold text-red-500 text-3xl text-center m-3	'>${product.price}</h1>
+                                <img src={product.image} alt="" width={200} className='h-60'  />
                                 <button
-                                    className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105'
+                                    className='bg-blue-500 hover:bg-blue-700 m-10 text-white font-bold p-3 rounded transition duration-300 ease-in-out transform hover:scale-105  '
                                     onClick={() => AddtoCart(product)}
                                 > 
-                                    Add to Cart
+                                    Add to Cart 
                                 </button>
                             </div>
-                        ))}
+                        ))} 
                         
                     </>
                 } /> 
--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 
-const Cart = ({cart,DeleteCart}) => {
+const Cart = ({ cart, DeleteCart }) => {
+  
   return (
+  
     <div className='w-full h-full flex flex-wrap'> 
        {
               cart.map((dame ,k) => ( 
-                  <div key={k} className='w-full sm:w-1/2  md:w-1/3 p-2 lg:w-1/4'> 
+                <div key={k} className='w-full sm:w-1/2 justify-center md:w-1/3 lg:w-1/4 p-10 transition duration-500 ease-in-out transform scale-75 hover:fill-white hover:scale-90 border border-gray-900'> 
+                  <h2 className='text-3xl font-sans font-bold italic'>{dame.category}</h2>
+                  <h1 className='font-bold text-red-500 text-3xl text-center m-3	'>${dame.price}</h1>
                       <img src={dame.image} alt="" width={200} />
-                      <button  className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105'
- onClick={()=>DeleteCart(k)}>Delete</button>
-                  </div>
+                      <button  className='bg-blue-500 hover:bg-blue-700 text-white font-bold p-4 rounded transition duration-300 ease-in-out transform hover:scale-105 m-11'
+                    onClick={() => DeleteCart(k)}>Delete</button>
+                  
+                
+                </div>
+                
               ))
-          }
+        
+      }
+     
     </div>
   )
 }
