8f1d7c4cbc909ade3743fe6e20ee917acb13af2e
Ankit Mod <ankit300mod@gmail.com>
2024-08-15 20:14:11 +0530

    data pushed

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -3,17 +3,20 @@ import React from 'react'
 const Cart = ({ cart, DeleteCart,total }) => {
   let Price = total().toFixed(2);
   return (
-    <div>
+    
       
-                      <h1 className='text-3xl font-sans font-bold italic md:text-5xl '>TOTAL PRICE : ${Price}</h1>
       
-        
+    <>
+                  <h1 className='text-3xl font-sans font-bold italic md:text-5xl '>TOTAL PRICE : ${Price}</h1>
+
     <div className='w-full h-full flex flex-wrap'>
+
+      
     
       {
         
               cart.map((dame ,k) => ( 
-                <div key={k} className='w-full sm:w-1/2 justify-center md:w-1/3 lg:w-1/4 p-10 transition duration-500 ease-in-out transform scale-75 hover:fill-white hover:scale-90 border border-gray-900'> 
+                <div key={k} className='w-full sm:w-1/2 justify-center md:w-1/3 lg:w-1/4 p-10 transition duration-500 ease-in-out transform scale-75 hover:fill-white hover:scale-90 border border-gray-900 mt-11 sm:justify-center'> 
                   <h2 className='text-3xl font-sans font-bold italic'>{dame.category}</h2>
                   <h1 className='font-bold text-red-500 text-3xl text-center m-3	'>${dame.price}</h1>
                       <img src={dame.image} alt="" width={200} />
@@ -27,10 +30,10 @@ const Cart = ({ cart, DeleteCart,total }) => {
           ))
           
         
-      }
+      } 
 
-      </div>
-      </div>
+</div>
+      </>
   )
 }
 
